e92b8ce11b872d5f6f4d3ed465a12733b16f3fbd
Goran Mrzljak <goran.mrzljak@gmail.com>
2013-11-23 23:48:18 +0100

    Updated some license text.

--- helljump/src/com/turbogerm/helljump/screens/ScreenBase.java
+++ helljump/src/com/turbogerm/helljump/screens/ScreenBase.java
@@ -1,20 +1,5 @@
-/*
- * Copyright 2012 Gustavo Steigert
- * 
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-// I made modifications to Gustavo Steigert's AbstractScreen class
-// http://steigert.blogspot.com/2012/02/2-libgdx-tutorial-game-screens.html
+// based on AbstractScreen from http://steigert.blogspot.com/2012/02/2-libgdx-tutorial-game-screens.html (by Gustavo Steigert)
+// modified by Goran Mrzljak
 package com.turbogerm.helljump.screens;
 
 import com.badlogic.gdx.Gdx;
